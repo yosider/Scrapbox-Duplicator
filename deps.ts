8eec0a80fc19dcab34eca46f0b701632b2bf109f
yosider <yosider.on.earth@gmail.com>
2025-08-11 04:26:21 +0900

    format code; update settings

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
-import 'https://deno.land/x/dotenv@v3.2.2/load.ts';
-export { assertString } from 'https://deno.land/x/unknownutil@v3.18.1/mod.ts';
-export * from 'https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.14/rest/page-data.ts';
+import "https://deno.land/x/dotenv@v3.2.2/load.ts";
+export { ensure } from "@core/unknownutil/ensure";
+export { isString } from "@core/unknownutil/is/string";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.16/rest/page-data.ts";
