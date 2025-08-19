b1b9305837daf49b6c898d13c62c126be13c7d6c
GitHub <noreply@github.com>
2025-08-19 00:53:20 +0000

    :arrow_up: update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { ensure } from "@core/unknownutil/ensure";
 export { isString } from "@core/unknownutil/is/string";
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.16/rest/page-data.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.30.2/rest/page-data.ts";
